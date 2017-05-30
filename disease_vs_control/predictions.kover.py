1a6044d7cfd11f4304082fc57d5043879f435f1d
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2017-05-30 10:45:41 -0400

    updated kover pred code

--- disease_vs_control/predictions.kover.py
+++ disease_vs_control/predictions.kover.py
@@ -11,7 +11,7 @@ n_cpu = 4
 phenotype_name = "Epilepsy case vs control"
 genomic_data_path = "/exec5/GROUP/pacoss/COMMUN/claudia/machine_learning/data/ibd_clusters.kmers.matrix"
 phenotype_data_path = "/exec5/GROUP/pacoss/COMMUN/claudia/machine_learning/data/ibd_clusters.pheno.txt"
-kover_dataset_path = "/exec5/GROUP/pacoss/COMMUN/adrouin/epilepsie/data/disease_vs_control/dataset.kover"
+kover_dataset_path = "./data/dataset.kover"  #"/exec5/GROUP/pacoss/COMMUN/adrouin/epilepsie/data/disease_vs_control/dataset.kover"
 
 
 # Create the Kover dataset if needed
@@ -46,18 +46,27 @@ kover dataset split \
 """.format(kover_dataset_path, split, " ".join(train_ids), " ".join(test_ids)))
 
 # Run the experiments
+if not os.path.exists(os.path.join("predictions", "kover.scm")):
+    os.mkdir(os.path.join("predictions", "kover.scm"))
+
 for split in os.listdir("splits"):
     output_dir = os.path.join("predictions", "kover.scm", split)
-    os.system(
+
+    if not os.path.exists(os.path.join(output_dir, "results.json")):
+        os.system(
 """
 kover learn \
     --dataset {0!s} \
     --split {1!s} \
     --model-type conjunction disjunction \
+    --p 1.0 1.4678 2.1544 3.1623 4.6416 6.8129 10.0 14.678 21.5443 31.6228 \
+    --max-rules 30 \
     --hp-choice cv \
     --random-seed 42 \
     --n-cpu {2:d} \
     --output-dir {3!s} \
-    -v
+    -x
 """.format(kover_dataset_path, split, n_cpu, output_dir))
-    
+
+        # TODO: Save the predictions in the same format as the sklearn algorithms
+
